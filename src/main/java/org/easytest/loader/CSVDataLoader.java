6a4c35e88906b97106a22477f6cfa7316e540fb4
ravi-polampelli <ravi.polampelli@gmail.com>
2012-08-29 13:30:30 +0200

    Issue # 5 - Replace isEmpty() for JDK 1.5 compatibility

--- src/main/java/org/easytest/loader/CSVDataLoader.java
+++ src/main/java/org/easytest/loader/CSVDataLoader.java
@@ -126,7 +126,8 @@ public class CSVDataLoader implements Loader{
         while (csvReader.readRecord()) {
             Map<String, Object> actualData = new HashMap<String, Object>();
             String[] splitValues =csvReader.getValues();
-            if (splitValues.length > 0 && splitValues[0].isEmpty()) {
+            if (splitValues.length > 0 && "".equals(splitValues[0])) {
+                //splitValues[0].isEmpty() method is replaced for JDK 1.5 compatibility
                 isKeyRow = false;
             } else {
                 isKeyRow = true;
